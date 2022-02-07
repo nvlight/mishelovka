6b066f1acaa6eced64fa806e442685d58a12b557
nvlight <lightstyle11@mail.ru>
2022-02-07 14:57:01 +0300

    add media queries for show_catalog

--- resources/views/catalog/show_catalog/index.blade.php
+++ resources/views/catalog/show_catalog/index.blade.php
@@ -9,16 +9,36 @@
             @php
                 //dump($childs)
             @endphp
+            <style>
+                .items-ul {
+                    justify-content: space-between;
+                }
+                .widthClass{
+                    width: 300px;
+                    margin: 5px;
+                }
+                @media (max-width: 768px) {
+                    .items-ul{
+                        flex-wrap: nowrap !important;
+                        flex-direction: column;
+                    }
+                    .items-ul li{
+                        margin: 0;
+                        align-self: center;
+                        padding-top: 5px;
+                    }
+                }
+            </style>
             @if ($childs->count())
+                <ul class="items-ul d-flex flex-wrap p-0 m-0" style="list-style-type: none;">
                 @foreach($childs as $k => $v)
-                    <ul class="list-group-numbered">
-                        <li style="width: 300px; height: 300px; position: absolute;">
-                            <p>{{$parentCatRuss}} {{$v->price}} {{$v->size}}</p>
-                            <p>{{$v->img_filename}}</p>
-                            <img src="{{ asset('storage/'.$v->img) }}" alt="" style="width: 100%;">
-                        </li>
-                    </ul>
+                    <li class="p-lg-2 widthClass" style="">
+{{--                        <p>{{$parentCatRuss}} {{$v->price}} {{$v->size}}</p>--}}
+{{--                        <p>{{$v->img_filename}}</p>--}}
+                        <img src="{{ asset('storage/'.$v->img) }}" alt="" style="width: 100%;">
+                    </li>
                 @endforeach
+                </ul>
             @endif
         </div>
 
